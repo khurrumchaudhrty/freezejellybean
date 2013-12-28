74eef88a1127cb370c3984aff3c74deeda79685f
Khurrum Chaudhry <khurrum.mehmood@gmail.com>
2013-12-27 20:09:48 -0500

    icons to be used from theme and branding library project

--- freezejellybean/src/com/khurrum/freezejellybean/SplashActivity.java
+++ freezejellybean/src/com/khurrum/freezejellybean/SplashActivity.java
@@ -1,22 +1,24 @@
 package com.khurrum.freezejellybean;
 
+import com.actionbarsherlock.app.SherlockActivity;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
-public class SplashActivity extends Activity {
+public class SplashActivity extends SherlockActivity  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(com.actionbarsherlock.view.MenuItem menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.splash, menu);
+        getMenuInflater().inflate(R.menu.splash, (Menu) menu);
         return true;
     }
     
